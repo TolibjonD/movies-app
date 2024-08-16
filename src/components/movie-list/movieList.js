da80f158a02abba96f29d2d52751871f35ff30eb
Saidkodirov <stolibjon123@gmail.com>
2024-08-16 14:13:56 +0500

    fixed

--- src/components/movie-list/movieList.js
+++ src/components/movie-list/movieList.js
@@ -3,20 +3,22 @@ import MovieListItem from "../movie-list-item/movie-list-item"
 
 const MovieList = ({ data, onDelete, onToggleProp }) => {
 
-        return <ul className="movieList">
-            
-            { data.map(item => 
-                <MovieListItem
-                key={item.id}
-                name={item.name}
-                views={item.views}
-                favourite={item.favourite}
-                like={ item.like }
-                onDelete={ () => onDelete(item.id) }
-                onToggleProp= {(e) => onToggleProp(item.id, e.currentTarget.getAttribute("data-toggle"))}
-                />
-            ) }
-        </ul>
+    const noData = data.length > 0 ? false : true
+
+    return <ul className="movieList">
+        { noData && "No Movies Found... 😔" }
+        { data.map(item => 
+            <MovieListItem
+            key={item.id}
+            name={item.name}
+            views={item.views}
+            favourite={item.favourite}
+            like={ item.like }
+            onDelete={ () => onDelete(item.id) }
+            onToggleProp= {(e) => onToggleProp(item.id, e.currentTarget.getAttribute("data-toggle"))}
+            />
+        ) }
+    </ul>
     }
 
 export default MovieList
